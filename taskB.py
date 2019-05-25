3f11a79d831473d5a60bd688ed1140cfb039717c
Tobias Stachl <tobias.stachl@gmail.com>
2019-05-25 13:47:58 +0200

    taskB ready. added spei calculation

--- taskB.py
+++ taskB.py
@@ -62,33 +62,93 @@ def calc_spi(da):
     return stacked
 
 
-def plot_spi(stacked, var_name, aggr, save):
+def plot_index(stacked, index_name, var_names, aggr, save, out_dir):
 
-    SPI_ts = stacked['SPI']
+    ts = stacked[index_name]
 
-    ax = SPI_ts.plot(title='SPI time series ({})'.format(aggr), figsize=(10, 5))
+    out_dir = os.path.join(out_dir, 'taskB', index_name)
+    try:
+        os.makedirs(out_dir)
+    except FileExistsError:
+        pass
+
+    ax = ts.plot(title='{} time series ({})'.format(index_name, aggr), figsize=(10, 5))
     ax.axhline(0, linestyle='--', color='grey', alpha=0.5)
-    min = SPI_ts.min() * 1.1
-    max = SPI_ts.max() * 1.1
+    min = ts.min() * 1.1
+    max = ts.max() * 1.1
     ax.set_ylim(min, max)
     plt.tight_layout()
 
+    if len(var_names) > 1:
+        label_var_names = '_'.join(var_names)
+    else:
+        label_var_names = var_names[0]
+
     if save:
-        SPI_ts.to_csv(os.path.join(out_dir, 'taskB', '{}_basin_avg_SPI_{}.csv'.format(aggr, var_name)), header=False)
+        ts.to_csv(os.path.join(out_dir, '{}_basin_avg_{}_{}.csv'.format(aggr, index_name, label_var_names)),
+                      header=False)
         plt.savefig(
-            os.path.join(out_dir, 'taskB', '{}_SPI_{}.png'.format(aggr, var_name)),
+            os.path.join(out_dir, '{}_{}_{}.png'.format(aggr, index_name, label_var_names)),
             bbox_inches="tight")
         plt.close()
     else:
         plt.show()
 
 
-def calc_spei(da):
+def calc_spei(da_prec, da_pet):
     """
-    calculate SPEI
+    calculate SPEI, which is difference between precipitation
+    and potential evapotranspiration
     """
-    # TODO: implement SPEI (P - ETp) -> should be the same apart from the input
-    pass
+    # name of variable
+    var_name_prec = da_prec.var().name
+    var_name_pet = da_pet.var().name
+
+    # only select pet data where also prec data is available
+    da_pet = da_pet.sel(time=da_prec.time)
+
+    basin_mean_prec = da_prec.groupby('time').mean().to_dataframe()
+    basin_mean_pet = da_pet.groupby('time').mean().to_dataframe()
+
+    merged = pd.concat([basin_mean_prec, basin_mean_pet], axis=1).dropna()
+    merged['DIFF'] = merged[var_name_prec] - merged[var_name_pet]
+
+    diff = merged['DIFF']
+
+    gammaparams = (diff.groupby(diff.index.month).apply(stats.gamma.fit))
+    ppf_per_month = {}
+
+    # get ppf for each month separately
+    for month in gammaparams.index.values:
+        values_of_month = diff[diff.index.month == month]
+        a, loc, scale = gammaparams[month]
+        cdf = stats.gamma.cdf(values_of_month, a, loc, scale)
+        ppf = stats.norm.ppf(cdf)
+        ppf_per_month[month] = ppf
+
+    # Reconstruction of time series (kann man sicher sauberer/flotter machen)
+    # -------------------------------------------------------------------------
+
+    # create a year index
+    idx = np.arange(diff.index.year[0], diff.index.year[-1] + 1)
+
+    # ppf dict to data frame
+    df = pd.DataFrame.from_dict(ppf_per_month).transpose()
+
+    # reindex to years
+    df.columns = idx
+    df = df.transpose()
+
+    # stack and reset the index
+    stacked = df.stack().reset_index()
+
+    # create index based on year and month multiindex
+    new_idx = pd.to_datetime(stacked.level_0.astype('str') + '-' + stacked.level_1.astype('str') + '-01',
+                             format='%Y-%m-%d')
+    stacked = stacked.set_index(new_idx)
+    stacked = stacked.rename(columns={0: 'SPEI'})
+
+    return stacked
 
 
 if __name__ == '__main__':
@@ -104,20 +164,27 @@ if __name__ == '__main__':
     fname_merged = os.path.join(data_dir, 'RAINFALL_MERGED_monthly_0d25.nc')
     da_trmm = utils.read_nc(fname_merged, 'TRMM')
 
-    """
     fname = os.path.join(data_dir, 'GLEAM_PET_monthly_0d25.nc')
     da_pet = utils.read_nc(fname, 'PET')
     da_pet = da_pet * (30.)
     da_pet.attrs['units'] = 'mm/month'
 
+    """
     fname = os.path.join(data_dir, 'GLEAM_AET_monthly_0d25.nc')
     da_aet = utils.read_nc(fname, 'AET')
     da_aet = da_aet * (30.)
     da_aet.attrs['units'] = 'mm/month'
     """
-    stacked_trmm = calc_spi(da_trmm)
-
     aggr = '48M'
-    stacked_trmm = stacked_trmm.resample(aggr).sum()
 
-    plot_spi(stacked_trmm, 'TRMM', aggr, True)
+    # calc and plot SPI with TRMM
+    # stacked_spi = calc_spi(da_trmm)
+    # stacked_spi = stacked_spi.resample(aggr).sum()
+    # plot_index(stacked_spi, 'SPI', ['TRMM'], aggr, True, out_dir)
+
+    # calc and plot SPEI with TRMM and GLEAM PET
+    stacked_spei = calc_spei(da_trmm, da_pet)
+    stacked_spei = stacked_spei.resample(aggr).sum()
+    plot_index(stacked_spei, 'SPEI', ['TRMM', 'GLEAM'], aggr, True, out_dir)
+
+
